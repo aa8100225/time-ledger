3ceb535e77f4905510fc42be712f3a39921f433a
Ian Dai <ian.dai@cyberstar.com.tw>
2024-05-29 21:11:07 +0800

    feat(about): add About page and update project configuration
    - Added a new About page with sections for Welcome, Feedback & Collaboration, Additional Tools, and Development Tips. Updated the layout and footer components for better styling and structure. Adjusted configuration settings.
    
    - Updated .gitignore to stop tracking .env and the analyze directory. Removed .env and analyze/nodejs.html from tracking to prevent sensitive data and unnecessary files from being included in version control.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const locales = ["en", "zh-TW"] as const;
 export const defaultLocale = "en" as const;
-export const baseAllowedPaths = ["/", "/others"] as const;
+export const baseAllowedPaths = ["/", "/others", "/about"] as const;
 
 export type Locale = (typeof locales)[number];
