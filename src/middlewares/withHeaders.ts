66f013ec4bed8972080b9d19f223720ed73e1501
Ian <ian.dai.wk@gmail.com>
2024-06-01 00:53:15 +0800

    fix: resolve CSP issue for Google Analytics
    Updated Content Security Policy (CSP) to allow Google Analytics scripts to execute properly. This involved modifying the `script-src`

--- src/middlewares/withHeaders.ts
+++ src/middlewares/withHeaders.ts
@@ -7,16 +7,16 @@ export const withHeaders: MiddlewareFactory = (next: NextMiddleware) => {
     const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'nonce-${nonce}';
+      script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
       style-src 'self' 'nonce-${nonce}' ;
-      img-src 'self' blob: data:;
+      img-src 'self' https://www.googletagmanager.com blob: data:;
       font-src 'self';
       object-src 'none';
       base-uri 'self';
       form-action 'self';
       frame-ancestors 'none';
       upgrade-insecure-requests;
-      connect-src 'self';
+      connect-src 'self' https://www.googletagmanager.com;
   `
       .replace(/\s{2,}/g, " ")
       .trim();
